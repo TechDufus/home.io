6354b788e9eb2e0d2e75f6dfd56c6affb8c633af
Matthew DeGarmo <matthewjdegarmo@gmail.com>
2022-06-21 08:11:06 -0500

    Restructure of flux files. Adding watchdog config / reload.

--- ansible/roles/flux/files/update_flux.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-alias update-flux='sudo apt-get update -y && sudo apt-get --with-new-pkgs upgrade -y && sudo apt autoremove -y && cd /home/techdufus/zelflux && git checkout . && git checkout master && git reset && git pull && sudo reboot'
